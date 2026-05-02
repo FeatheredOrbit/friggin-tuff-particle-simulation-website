6addc9d355081f5b4309ac648b8701f4ec188fe2
Feathered Orbit <featheredorbit.dev@gmail.com>
2026-05-02 22:10:10 +0100

    Name changes and comments

--- src/canvas-controls.ts
+++ src/canvas-controls.ts
@@ -11,17 +11,21 @@ const pinch = new Hammer.Pinch({
 const rotate = new Hammer.Rotate({
   direction: Hammer.DIRECTION_ALL,
   enable: true,
-  threshold: 15
+  threshold: 10
 });
 
 pinch.recognizeWith(rotate);
-
 hammerManager.add([pinch, rotate]);
 
 let currentScale= 1;
 let currentRotation= 0;
 
 hammerManager.on("rotatestart", (val) => {
+  // For some reason when first placing your fingers down to start rotating,
+  // a very large number is returned for rotation? Like in my case every time
+  // I placed my fingers down, it applied a random rotation before properly
+  // rotating. My solution was to subtract it in rotatestart so that it cancels
+  // out with the one in rotatemove.
   currentRotation -= val.rotation;
 });
 
